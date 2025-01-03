08e7f1e99ee8d36946bdb1b030ea6e0d56dcde45
Vinay Singh <vinaysthakur15@gmail.com>
2025-01-03 18:42:00 +0530

    Repeated Code Changes

--- Microsoft/ImageSmoother.cpp
+++ Microsoft/ImageSmoother.cpp
@@ -52,32 +52,4 @@ vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
         }
 
         return ans;
-    }vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
-        int m = img.size();
-        int n = img[0].size();
-        vector<vector<int>> ans(m, vector<int>(n));
-
-        vector<pair<int, int>> directions = {{-1, -1}, {-1, 0}, {-1, 1},
-                                             {0, -1},  {0, 0},  {0, 1},
-                                             {1, -1},  {1, 0},  {1, 1}};
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int sum = 0;
-                int count = 0;
-                for(auto dir: directions){
-                    int x = i + dir.first;
-                    int y = j + dir.second;
-
-                    if(x>=0 && x<m && y>=0 && y<n){
-                        sum += img[x][y];
-                        count++;
-                    }
-                }
-
-                ans[i][j] = floor(sum/count);
-            }
-        }
-
-        return ans;
     }
