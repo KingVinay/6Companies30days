24e62fbeea042f0b1e145a9d1e94a8fdef12d38b
Vinay Singh <vinaysthakur15@gmail.com>
2025-01-17 20:43:36 +0530

    Friends Appropriate Linear Time Solution

--- Walmart/FriendsofAppropriateAges.cpp
+++ Walmart/FriendsofAppropriateAges.cpp
@@ -1,6 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// O(n) Solution Using Iterating Over Range of age
+
+int numFriendRequests(vector<int>& ages) {
+        vector<int> FriendsAges(121, 0);
+
+        for (int age : ages) {
+            FriendsAges[age]++;
+        }
+
+        int ans = 0;
+
+        for (int ageA = 1; ageA <= 120; ++ageA) {
+            int countAgeA = FriendsAges[ageA];
+
+            for (int ageB = 1; ageB <= 120; ++ageB) {
+                int countAgeB = FriendsAges[ageB];
+
+                if (!(ageB <= 0.5 * ageA + 7 || ageB > ageA ||
+                      (ageB > 100 && ageA < 100))) {
+                    ans += countAgeA * countAgeB;
+
+                    if (ageA == ageB) {
+                        ans -= countAgeB;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+
 // O(n^2) Solution using 2 nested loops
 
 int numFriendRequests(vector<int>& ages) {
@@ -26,7 +57,7 @@ int numFriendRequests(vector<int>& ages) {
         }
 
         return ans;
-    }
+}
 
 int main()
 {
